e5d12eb74eb9392d501e183e255a5eb14557565a
blarso236 <94015436+blarso236@users.noreply.github.com>
2022-02-10 15:16:26 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+var i = 0;
+function move() {
+    if (i == 0) {
+        i = 1;
+        var elem = document.getElementById("myBar");
+        var width = 10;
+        var id = setInterval(frame, 10);
+        function frame() {
+            if (width >= 100) {
+                clearInterval(id);
+                i = 0;
+            } else {
+                width++;
+                elem.style.width = width + "%";
+                elem.innerHTML = width + "%";
+            }
+        }
+    }
+}
+
+function mouseOver() {
+    document.getElementById("A").style.backgroundColor = "blue";
+}
+
+function mouseOut() {
+    document.getElementById("A").style.backgroundColor = "darkgray";
+}
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
+
+$( function() {
+  $( "#datepicker" ).datepicker();
+} );
